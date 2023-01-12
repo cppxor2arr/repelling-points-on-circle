e4ba4947d2f4f5ceefd67a151e8efb8d216bd7d1
cppxor2arr <>
2023-01-12 22:54:05 +0900

    use CMake

--- helpers.cpp
+++
@@ -1,9 +0,0 @@
-#include <SFML/System/Vector2.hpp>
-#include <cmath>
-
-float get_touch_angle(const sf::Vector2f &touch_pos,
-                      const sf::Vector2f &origin)
-{
-    const auto rel_pos = touch_pos - origin;
-    return std::atan2(rel_pos.y, rel_pos.x);
-}
--- helpers.hpp
+++
@@ -1,4 +0,0 @@
-#include <SFML/System/Vector2.hpp>
-
-float get_touch_angle(const sf::Vector2f &touch_pos,
-                      const sf::Vector2f &origin);
